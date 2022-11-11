042381a51255b97f696011380551db797e0f71c8
Mehdi <sadour.mehdi@gmail.com>
2022-11-11 23:51:13 +0100

    Setup database.

--- collabs_api/settings.py
+++ collabs_api/settings.py
@@ -84,10 +84,14 @@ WSGI_APPLICATION = 'collabs_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "collabs",
+            "USER": "postgres",
+            "PASSWORD": "qwertz",
+            "HOST": "localhost",
+            "PORT": 5433,
+        }
 }
 
 
