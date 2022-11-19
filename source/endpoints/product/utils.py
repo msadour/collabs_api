24a7939d7678bca1c03973d0ffffcd06daf6441a
Mehdi <sadour.mehdi@gmail.com>
2022-11-19 03:19:13 +0100

    add update/delete product endpoint.

--- source/endpoints/product/utils.py
+++ source/endpoints/product/utils.py
@@ -1,1 +1,60 @@
 """Utils file."""
+from typing import Any
+
+from source.endpoints.product.models import Product, Category
+from source.layer.common.utils import get_object_or_none
+
+
+class ActionProduct:
+    """Class ActionProduct."""
+
+    def __init__(self, request):
+        self.data = request.data
+        self.user = request.user
+
+    @property
+    def get_product(self) -> Any:
+        """Get product from data.
+
+        Returns:
+            Product.
+        """
+        return get_object_or_none(
+            model_class=Product, id_to_retrieve=self.data.get("id")
+        )
+
+    def check_owner_product(self) -> bool:
+        """Check if current user is owner of the product.
+
+        Returns:
+            Is owner or not.
+        """
+        return self.get_product.proposer == self.user
+
+    def update_product(self) -> None:
+        """Update product from data sent by server."""
+        product = self.get_product
+        for attr, value in self.data.items():
+            if attr in ["label", "quantity", "description"]:
+                setattr(product, attr, value)
+
+        product.save()
+
+    def delete_product(self) -> None:
+        """Delete product."""
+        if self.get_product.proposer == self.user:
+            self.get_product.delete()
+
+    def publish_product(self) -> None:
+        """Publish a product."""
+        category = get_object_or_none(
+            model_class=Category, id_to_retrieve=self.data.get("category", None)
+        )
+
+        Product.objects.create(
+            label=self.data.get("label"),
+            quantity=self.data.get("quantity"),
+            description=self.data.get("description"),
+            proposer=self.user,
+            category=category,
+        )
--- source/endpoints/product/views.py
+++ source/endpoints/product/views.py
@@ -1,15 +1,17 @@
 """Views account module."""
 
+from django.forms.models import model_to_dict
+
 from rest_framework import viewsets, status
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from source.endpoints.product.models import Product, Category
-from source.layer.common.utils import get_object_or_none
+from source.endpoints.product.models import Product
+from source.endpoints.product.utils import ActionProduct
 
 
 class ProductViewSet(viewsets.ViewSet):
-    """Class AccountViewSet."""
+    """Class ProductViewSet."""
 
     def create(self, request: Request) -> Response:
         """Create a product.
@@ -20,19 +22,59 @@ class ProductViewSet(viewsets.ViewSet):
         Returns:
             Response from the server.
         """
-        data = request.data
-        user = request.user
+        if bool(request.user and request.user.is_authenticated):
+            return Response(status=status.HTTP_403_FORBIDDEN)
 
-        category = get_object_or_none(
-            model_class=Category, id_to_retrieve=data.get("category", None)
-        )
-
-        Product.objects.create(
-            label=data.get("label"),
-            quantity=data.get("quantity"),
-            description=data.get("description"),
-            proposer=user,
-            category=category,
-        )
-
+        action = ActionProduct(request=request)
+        action.publish_product()
         return Response(status=status.HTTP_201_CREATED)
+
+    def list(self, request: Request) -> Response:
+        """List all product.
+
+        Args:
+            request: request sent by the client.
+
+        Returns:
+            Response from the server with list of users.
+        """
+        data: list = [
+            model_to_dict(product) for product in Product.objects.filter(available=True)
+        ]
+        return Response(data, status=status.HTTP_200_OK)
+
+    def patch(self, request: Request):
+        """Update a product.
+
+        Args:
+            request: request sent by the client.
+
+        Returns:
+            Response from the server.
+        """
+        action = ActionProduct(request=request)
+
+        if not action.check_owner_product():
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        action.update_product()
+
+        return Response(status=status.HTTP_200_OK)
+
+    def delete(self, request: Request):
+        """Delete a product.
+
+        Args:
+            request: request sent by the client.
+
+        Returns:
+            Response from the server.
+        """
+        action = ActionProduct(request=request)
+
+        if not action.check_owner_product():
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        action.delete_product()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
